49e9c74e11e25b961402bb9c8efc06ff7c4278d6
Andrew O'Hara <ohara.andrew314@gmail.com>
2024-08-04 20:25:00 -0400

    publish to central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+import com.vanniktech.maven.publish.KotlinJvm
+
 plugins {
     kotlin("jvm")
     id("maven-publish")
+    id("com.vanniktech.maven.publish")
+    id("jacoco")
 }
 
 repositories {
@@ -11,14 +16,14 @@ dependencies {
     implementation(platform("org.http4k:http4k-connect-bom:_"))
 
     api("com.nimbusds:nimbus-jose-jwt:_")
-    api("org.http4k:http4k-connect-amazon-kms")
+    api("org.http4k:http4k-connect-amazon-kms:_")
 
     testImplementation(kotlin("test"))
-    testImplementation("org.http4k:http4k-connect-amazon-kms-fake")
+    testImplementation("org.http4k:http4k-connect-amazon-kms-fake:_")
     testImplementation("io.kotest:kotest-assertions-core-jvm:_")
     testImplementation("org.bouncycastle:bcprov-jdk18on:_")
     testImplementation("org.bouncycastle:bcpkix-jdk18on:_")
-    testImplementation("dev.forkhandles:result4k-kotest")
+    testImplementation("dev.forkhandles:result4k-kotest:_")
 }
 
 tasks.test {
@@ -35,23 +40,33 @@ tasks.compileKotlin {
     }
 }
 
-tasks {
-    val sourcesJar by creating(Jar::class) {
-        archiveClassifier.set("sources")
-        artifacts {
-            kotlinSourcesJar
+mavenPublishing {
+    configure(KotlinJvm(sourcesJar = true))
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+    signAllPublications()
+    coordinates("dev.andrewohara", "nimbus-kms", "0.1")
+
+    pom {
+        name.set("Nimbus KMS")
+        description.set("Collection of useful kotlin microservice utilities")
+        inceptionYear.set("2023")
+        url.set("https://github.com/oharaandrew314/nimbus-kms")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
         }
-    }
-
-    artifacts {
-        archives(sourcesJar)
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["kotlin"])
+        developers {
+            developer {
+                id.set("oharaandrew314")
+                name.set("Andrew O'Hara")
+                url.set("https://github.com/oharaandrew314")
+            }
+        }
+        scm {
+            url.set("https://github.com/oharaandrew314/nimbus-kms")
         }
     }
 }
